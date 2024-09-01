96b38f3ac5dd42bea6470841094692123492ecb9
Papireddy <papireddyeppala903@gmail.com>
2024-09-01 12:03:37 +0530

    graph in adjlist

--- graph_in_adjlist.cpp
+++ graph_in_adjlist.cpp
@@ -3,34 +3,34 @@
 #include<unordered_map>
 using namespace std; 
 
-void display(unordered_map<int,vector<int>> mp){
-    for (auto it:mp){
-        cout << it.first << " -> ";
-        for (auto neighbor : it.second){
-            cout << neighbor << " "; 
+void display(vector<vector<int>> &adj){
+    int n = adj.size();
+    for (int i = 0;i<n;i++){
+        cout << i << "->";
+        for (auto neighbor : adj[i]){
+            cout << neighbor << " ";
         }
         cout << endl;
     }
 }
 
 int main(){
-    unordered_map<int, vector<int>>adjList;
-    
     int n, m;
     cout << "Number of vertices: ";
     cin >> n;  
     cout << "Number of edges: ";
     cin >> m;
+    vector<vector<int>> adj(n+1);    
 
     cout << "Enter the Edges " << endl;
     for (int i = 0;i<m;i++){
         int u,v;
         cin>>u>>v;
 
-        adjList[u].push_back(v);
-        adjList[v].push_back(u);
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
 
-    display(adjList);
+    display(adj);
     
 }
